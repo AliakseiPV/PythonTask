faa9e928a3c118cd6a396142c459eec1bad760b4
Aliaksei Poliak <aliakseipv@gmail.com>
2023-02-17 05:30:44 -0500

    Adding button_add

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import pickle
 root = tkinter.Tk()
 root.title("To-Do List")
 
+def add_task():
+    task = entry.get()
+    if task != "":
+        listbox.insert(tkinter.END, task)
+        entry.delete(0,tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(message="Enter a task.")
 
 frame = tkinter.Frame(root)
 frame.pack()
@@ -20,6 +27,9 @@ scrollbar.config(command=listbox.yview)
 
 entry = tkinter.Entry(root,width=50)
 entry.pack()
+
+button_add = tkinter.Button(root, text="Add", width=48, command=add_task)
+button_add.pack()
 
 
 root.mainloop()
